2216131560d8fc79e5d1a1139f673969bc9a9b05
Bernardo Dominguez <berny0996@gmail.com>
2025-02-17 00:12:54 -0500

    testing stock entry

--- src/test/java/com/deceptive/stock/entry/EntryTest.java
+++ src/test/java/com/deceptive/stock/entry/EntryTest.java
@@ -1,13 +1,21 @@
 package com.deceptive.stock.entry;
 
 import com.deceptive.stock.controller.EntryController;
+import com.deceptive.stock.exception.ResourceNotFoundException;
+import com.deceptive.stock.model.Entry;
+import com.deceptive.stock.model.Product;
+import com.deceptive.stock.repo.ProductRepo;
+import com.deceptive.stock.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(EntryController.class)
 public class EntryTest {
@@ -18,8 +26,18 @@ public class EntryTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private ProductRepo prodRepo;
+
     @Test
     void testCreateEntry_Success() throws Exception {
-        this.mockMvc.perform(post(""));
+        Product prod = prodRepo.findById(1)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "id", 1));
+        Entry entry = new Entry(prod,7);
+        this.mockMvc.perform(post("")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(prod)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.quantity").value(7))
+                .andExpect(jsonPath("$.product.name").value("Monitor"));
     }
 }
