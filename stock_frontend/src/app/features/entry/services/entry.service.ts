38982b2e6c50f36a3a89399faf768c749b4a94eb
deceptiveb <berny0996@gmail.com>
2025-03-04 22:11:51 -0500

    updated entry service on home

--- stock_frontend/src/app/features/entry/services/entry.service.ts
+++ stock_frontend/src/app/features/entry/services/entry.service.ts
@@ -1,18 +1,20 @@
-// src/app/services/user.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Entry } from '../models/entry.model';
+import { environment } from '../../../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/users'; // Example API
+  private apiUrl = environment.apiUrl; // Use environment variable
 
   constructor(private http: HttpClient) {}
 
+  // Fetching a list of users (Array of objects)
   getUsers(): Observable<Entry[]> {
+    var apiUrl = this.apiUrl + "/api/entry";
     return this.http.get<Entry[]>(this.apiUrl);
   }
 }
