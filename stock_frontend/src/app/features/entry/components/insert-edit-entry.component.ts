5a36904f9c87f9a318d49c866ce9e116a214c156
deceptiveb <berny0996@gmail.com>
2025-03-18 21:49:46 -0500

    Entry editor

---
+++ stock_frontend/src/app/features/entry/components/insert-edit-entry.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component(
+    {
+        selector: "app-edit-entry",
+        template: `
+        <div></div>
+        `,
+    }
+)
+
+export default class EditInsertEntryComponent {
+
+}
