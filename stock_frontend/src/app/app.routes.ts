6af87f3db2ec700b984af95f95873d95d679baf6
Bernardo Dominguez <berny0996@gmail.com>
2025-03-26 07:58:43 -0500

    File form front

--- stock_frontend/src/app/app.routes.ts
+++ stock_frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import EntryListComponent from './features/entry/components/entries-list.component';
 import HomeComponent from './features/entry/pages/home/home.component';
+import EditInsertEntryComponent from './features/entry/components/insert-edit-entry.component';
 
 export const routes: Routes = [
     {
         path: "",
         loadComponent: () => EntryListComponent,
+    },
+    {
+        path: "entry/edit",
+        loadComponent: () => EditInsertEntryComponent,
     }
 ];
--- stock_frontend/src/app/features/entry/components/insert-edit-entry.component.ts
+++ stock_frontend/src/app/features/entry/components/insert-edit-entry.component.ts
@@ -1,14 +1,46 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component(
     {
         selector: "app-edit-entry",
         template: `
-        <div></div>
+        <div>
+            <form (ngSubmit)="submitForm()">
+                <div>
+                    <label for="file">File</label>
+                    <input 
+                    type="file"
+                    id="file"
+                    (change)="onFileSelected($event)"
+                    accept=".jpg,.png">
+                </div>
+            </form>
+        </div>
         `,
     }
 )
 
 export default class EditInsertEntryComponent {
+    uploadForm: FormGroup;
+    uploadProgress:number = 0;
+    constructor(private fb: FormBuilder){
+        this.uploadForm = this.fb.group(
+            {
+                file: [null, Validators.required]
+            }
+        )
+    }
 
+    submitForm(){
+
+    }
+
+    onFileSelected(e: Event){
+        const fileInput = e.target as HTMLInputElement;
+        if(fileInput?.files?.length){
+            const file = fileInput.files[0];
+            this.uploadForm.patchValue({file})
+        }
+    }
 }
