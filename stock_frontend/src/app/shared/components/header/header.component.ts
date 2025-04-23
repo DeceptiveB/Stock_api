f3a3e0725366635c1770f1ac700359526e46fc56
deceptiveb <berny0996@gmail.com>
2025-04-23 08:13:22 -0500

    Header added to app

---
+++ stock_frontend/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+    selector: 'app-header',
+    styles: '',
+    templateUrl: './header.component.html',
+    imports: [RouterLink]
+})
+
+export default class HeaderComponent {
+
+}
