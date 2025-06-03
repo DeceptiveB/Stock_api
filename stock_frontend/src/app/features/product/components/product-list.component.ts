148264f8b71fcc9bfc0333128ea24c3317d9da85
deceptiveb <berny0996@gmail.com>
2025-06-03 08:10:38 -0500

    changed datatable for products to ngx-datatable

--- stock_frontend/src/app/features/product/components/product-list.component.ts
+++ stock_frontend/src/app/features/product/components/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProductListItem } from "../models/product-list-item.model";
 import { ProductService } from "../services/product.service";
 import { CommonModule } from "@angular/common";
 import { NotificationService } from "../../../shared/components/toast/services/notification.service";
-import NotificationComponent from "../../../shared/components/toast/components/notification.component";
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-product-list',
@@ -19,21 +20,46 @@ import NotificationComponent from "../../../shared/components/toast/components/n
             Cargando resultados
         </div>
         <div *ngIf="!loading && !isEmpty">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Brand</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                @for (product of products; track $index) {
-                        <tr app-product-list-item [product]="product"></tr>
-                }
-                </tbody>
-            </table>
+            <!---<table datatable [dtOptions]="dtOptions" class="table">--->
+            <!---    <thead>--->
+            <!---        <tr>--->
+            <!---            <th></th>--->
+            <!---            <th>Name</th>--->
+            <!---            <th>Brand</th>--->
+            <!---            <th></th>--->
+            <!---        </tr>--->
+            <!---    </thead>--->
+            <!---    <tbody>--->
+            <!---    @for (product of products; track $index) {--->
+            <!---            <tr app-product-list-item [product]="product"></tr>--->
+            <!---    }--->
+            <!---    </tbody>--->
+            <!---</table>--->
+            <ngx-datatable
+                class="ngx-datatable material"
+                [rows]="products"
+                [columns]="[{ name: 'Name' }, { name: 'Description' }, { name: 'Brand' }]"
+                [headerHeight]="50"
+                [footerHeight]="50"
+                rowHeight="auto"
+                [externalPaging]="false"
+                [count]="products.length"
+                [offset]="1"
+                [limit]="2"
+                [columnMode]="'flex'"
+            >
+            <!-- Name Column -->
+                <ngx-datatable-column [flexGrow]="1" name="Id" prop="id"></ngx-datatable-column>
+                <ngx-datatable-column [flexGrow]="2" name="Name" prop="name"></ngx-datatable-column>
+
+                <!-- Price Column -->
+                <ngx-datatable-column [flexGrow]="2" name="Description" prop="description"></ngx-datatable-column>
+                <ngx-datatable-column [flexGrow]="1" name="Actions" [sortable]="false">
+                    <ng-template let-row="row" let-rowIndex="rowIndex" ngx-datatable-cell-template>
+                        <a routerLinkActive="router-link-active" [routerLink]="['/product/edit', row.id]" class="btn btn-sm btn-primary me-2">Edit</a>
+                    </ng-template>
+                </ngx-datatable-column>
+            </ngx-datatable>
         </div>
         <div *ngIf="isEmpty">
             <h2>No hay resultados</h2>
@@ -41,14 +67,14 @@ import NotificationComponent from "../../../shared/components/toast/components/n
     </div>
     
     `,
-    imports: [ProductListItemComponent, CommonModule]
+    imports: [ProductListItemComponent, CommonModule, NgxDatatableModule, RouterLink]
 })
 
-export default class ProductListComponent{
+export default class ProductListComponent {
     products!: ProductListItem[];
     loading = true
     isEmpty = false
-    constructor(private productService: ProductService, private notificationService: NotificationService) {}
+    constructor(private productService: ProductService, private notificationService: NotificationService) { }
 
     ngOnInit(): void {
         const page = 0;
